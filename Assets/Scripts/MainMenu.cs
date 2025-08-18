43fd854f273282161f4f81efee145e6bc3299dc9
tangerine238 <stephentang238@gmail.com>
2025-08-18 18:25:17 -0400

    first push

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.MainGame);
+        });
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+
+        Time.timeScale = 1f;
+    }
+}
